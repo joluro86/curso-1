85465ef07f4ae1b2d8d737ea883f946a2e7d1021
joluro86 <joluro86@hotmail.com>
2025-11-12 16:43:25 -0500

    boton cambiar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Hola mundo con Angular';
-  otro ="Otro texto"
+  users: string[] = ['jorge', 'merlis', 'juan'];
+  visible: boolean = false;
+  texto = "Mostrar"
+
+  setVisible(): void {
+    this.visible = this.visible ? false : true;
+  }
 }
